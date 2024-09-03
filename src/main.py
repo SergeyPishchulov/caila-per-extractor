499aec86bc8a199cba6ca622baa9ff77830b239f
SergeyPishchulov <lcmrb26@bk.ru>
2024-09-03 08:41:52 +0500

    pydantic request & response

--- src/main.py
+++ src/main.py
@@ -1,21 +1,41 @@
+from typing import List
+
 from mlp_sdk.abstract import Task
 from mlp_sdk.hosting.host import host_mlp_cloud
 from mlp_sdk.transport.MlpServiceSDK import MlpServiceSDK
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 
 
 class PredictRequest(BaseModel):
-    name: str
+    texts: List[str]
 
-    def __int__(self, name):
-        self.name = name
+    @field_validator('texts')
+    @classmethod
+    def texts_are_not_too_long(cls, texts: List[str]) -> List[str]:
+        if sum([len(t) for t in texts]) > 1000:
+            raise ValueError("All texts should not be longer than 1000 characters")
+        return texts
+
+
+class Span(BaseModel):
+    start_index: int
+    end_index: int
+
+
+class Entity(BaseModel):
+    value: str
+    entity_type: str
+    span: Span
+    entity: str
+    source_type: str = "LLM_PER_EXTRACTOR"
+
+
+class EntityContainer(BaseModel):
+    entities: List[Entity]
 
 
 class PredictResponse(BaseModel):
-    response: str
-
-    def __int__(self, response):
-        self.response = response
+    entities_list: List[EntityContainer]
 
 
 class SimpleActionExample(Task):
